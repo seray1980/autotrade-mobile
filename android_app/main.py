5a7923be7df6e71872b05dee9abb40417aaf405e
seray1980 <13088276315@163.com>
2026-03-23 09:03:46 +0800

    fix: 修正App class名称 AutoTradeMobileApp -> MonitorApp

--- android_app/main.py
+++ android_app/main.py
@@ -1,5 +1,5 @@
 __version__ = '1.0.0'
 
 if __name__ == '__main__':
-    from AutoTradeMobile import AutoTradeMobileApp
-    AutoTradeMobileApp().run()
+    from AutoTradeMobile import MonitorApp
+    MonitorApp().run()
